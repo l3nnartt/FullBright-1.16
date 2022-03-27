e70af913a8f2627d2b45435b4620e055cf1c0f8f
l3nnartt <loesche.lennart@gmail.com>
2022-03-28 00:29:34 +0200

    refactor code + fix bug that on start fullbright is not activated

--- src/main/java/com/github/l3nnartt/fullbright/FullBright.java
+++ src/main/java/com/github/l3nnartt/fullbright/FullBright.java
@@ -1,6 +1,5 @@
 package com.github.l3nnartt.fullbright;
 
-import java.util.List;
 import net.labymod.api.LabyModAddon;
 import net.labymod.settings.elements.BooleanElement;
 import net.labymod.settings.elements.ControlElement;
@@ -8,29 +7,42 @@ import net.labymod.settings.elements.SettingsElement;
 import net.labymod.utils.Material;
 import net.minecraft.client.Minecraft;
 
+import java.util.List;
+
 public class FullBright extends LabyModAddon {
 
-  private boolean enabled;
+    private boolean enabled;
 
-  @Override
-  public void onEnable() {
-  }
+    @Override
+    public void onEnable() {
+        getLogger("Addon started");
+    }
 
-  @Override
-  public void loadConfig() {
-    this.enabled = !getConfig().has("enabled") || getConfig().get("enabled").getAsBoolean();
-  }
+    @Override
+    public void loadConfig() {
+        this.enabled = !getConfig().has("enabled") || getConfig().get("enabled").getAsBoolean();
+        if (enabled)
+            changeGama(10.0F);
+    }
 
-  @Override
-  protected void fillSettings(List<SettingsElement> list) {
-    BooleanElement enableFullBrightElement = new BooleanElement("Enable Full Bright", this, new ControlElement.IconData(Material.TORCH), "enableFullBright", this.enabled);
-    enableFullBrightElement.addCallback(callback -> {
-      if (callback) {
-        Minecraft.getInstance().gameSettings.gamma = 10.0F;
-      } else {
-        Minecraft.getInstance().gameSettings.gamma = 1.0F;
-      }
-    });
-    list.add(enableFullBrightElement);
-  }
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+        BooleanElement enableFullBrightElement = new BooleanElement("Enable Full Bright", this, new ControlElement.IconData(Material.TORCH), "enableFullBright", this.enabled);
+        enableFullBrightElement.addCallback(callback -> {
+            if (callback) {
+                changeGama(10.0F);
+            } else {
+                changeGama(1.0F);
+            }
+        });
+        list.add(enableFullBrightElement);
+    }
+
+    public void changeGama(double gamma) {
+        Minecraft.getInstance().gameSettings.gamma = gamma;
+    }
+
+    public static void getLogger(String log) {
+        System.out.println("[FullBright-1.16] " + log);
+    }
 }
